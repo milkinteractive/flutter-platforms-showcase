1b07fc329a28b0c45094814a1d8eeb6e625dad8f
Christoph Eck <christoph@milkinteractive.ch>
2022-09-20 11:55:54 +0200

    fix: inital commit

---
+++ android/app/src/main/kotlin/com/example/railvis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.railvis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RailVis',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Builder(builder: ((context) {
+        return SizedBox();
+      })),
+    );
+  }
+}
